7172defece0f65a6517a029c9441aab76f1e397d
C++ torturer <8353207@gmail.com>
2023-11-27 21:16:28 +0300

    fin

--- Project2/Source.cpp
+++ Project2/Source.cpp
@@ -23,24 +23,18 @@ public:
     
 };
 
-class Node2 
+class AVLNode
 {
 public:
-    Node2(int value, int key)
-    {
-        this->value = value;
-        this->key = key;
-        Left = nullptr;
-        Right = nullptr;
-        height = 0;
-    }
-    int value;
     int key;
     int height;
-    Node2* Left;
-    Node2* Right;
-   
+    AVLNode* Left;
+    AVLNode* Right;
+
+    AVLNode() : key(0), height(1), Left(nullptr), Right(nullptr) {}
+    AVLNode(int k) : key(k), height(1), Left(nullptr), Right(nullptr) {}
 };
+
 
 class BinaryTree
 {
@@ -51,6 +45,10 @@ public:
     BinaryTree()
     {
         root = nullptr;
+    }
+
+    Node* getRoot() {
+        return root;
     }
 
     void insert(int x)
@@ -77,21 +75,6 @@ public:
         return node;
     }
 
-    bool isBinaryTree(Node* node, int minValue, int maxValue)
-    {
-        if (node == nullptr)
-        {
-            return true;
-        }
-
-        if (node->data < minValue || node->data > maxValue)
-        {
-            return false;
-        }
-
-        return isBinaryTree(node->left, minValue, node->data - 1) &&
-            isBinaryTree(node->right, node->data + 1, maxValue);
-    }
 
     bool isValidInputChar(char ch)
     {
@@ -100,6 +83,8 @@ public:
 
     void buildTreeFromString(const string& str)
     {
+        int cpcount = 0;
+        int opcount = 0;
         stringstream ss(str);
         if (!isValidInput(ss.str()))
         {
@@ -107,7 +92,7 @@ public:
             return;
         }
 
-        buildTree(ss, root);
+        buildTree(ss, root,opcount,cpcount);
     }
 
     bool isValidInput(const string& str)
@@ -122,33 +107,67 @@ public:
         return true;
     }
 
-    void buildTree(stringstream& ss, Node*& node)
+    void buildTree(stringstream& ss, Node*& node, int& openParentheses, int& closeParentheses)
     {
         char ch;
         ss >> ch;
+
+        if (ch != '(')
+        {
+            cerr << "Error: Expected (" << endl;
+            return;
+        }
 
         // крафтим корень
         int value;
         ss >> value;
         node = new Node(value);
 
-        if (ss.eof() || ss.peek() == ')')
+        if (ss.eof())
         {
-            ss >> ch; // считываем )
+            cerr << "Error: Unexpected end of input" << endl;
             return;
         }
 
-        buildTree(ss, node->left);
-
-        if (ss.eof() || ss.peek() == ')')
+        if (ss.peek() == ')')
         {
             ss >> ch; // считываем )
+            closeParentheses++;
             return;
         }
 
-        buildTree(ss, node->right);
+        buildTree(ss, node->left, openParentheses, closeParentheses);
 
-        ss >> ch; // считываем )
+        if (ss.eof())
+        {
+            cerr << "Error: Unexpected end of input" << endl;
+            return;
+        }
+
+        if (ss.peek() == ')')
+        {
+            ss >> ch; // считываем )
+            closeParentheses++;
+            return;
+        }
+
+        buildTree(ss, node->right, openParentheses, closeParentheses);
+
+        if (ss.eof())
+        {
+            cerr << "Error: Unexpected end of input" << endl;
+            return;
+        }
+
+        if (ss.peek() == ')')
+        {
+            ss >> ch; // считываем )
+            closeParentheses++;
+        }
+        else
+        {
+            cerr << "Error: Expected )" << endl;
+        }
     }
 
     void DFS(Node* node)
@@ -170,40 +189,35 @@ public:
         cout << "\n";
     }
 
-    bool isValidBinaryTree()
-    {
-        return isBinaryTree(root, INT_MIN, INT_MAX);
-    }
+   
 };
+
 
 
 class AVLTree 
 {
 private:
-    Node2* root;
+    AVLNode* root;
 public:
     AVLTree()
     {
         root = nullptr;
     }
 
-
-    void CorrectHeight(Node2* node) {
-        int HeightLeft = GetSize(node->Left);
-        int HeightRight = GetSize(node->Right);
-        if (HeightLeft > HeightRight) {
-            node->height = HeightLeft + 1;
-        }
-        else
-            node->height = HeightRight + 1;
+    AVLNode* getRoot() {
+        return root;
     }
 
-    int BFactor(Node2* node) 
+    int max(int a, int b)
     {
-        return GetSize(node->Right) - GetSize(node->Left);
+        if (a < b) {
+            return b;
+        }
+        else return a;
 
     }
-    int GetSize(Node2* node) 
+
+    int GetSize(AVLNode* node) 
     {
         if (!node) 
         {
@@ -212,23 +226,327 @@ public:
         return node->height;
     }
 
-   /* Node2* balance(Node2* node) 
+    AVLNode* newNode(int key)
     {
-        CorrectHeight(node);
-        if (BFactor(node) == 2) {
-            if (BFactor(node->Right) < 0) 
-            {
-                node->Right = rotateRight(node->Right);
-            } 
-            return rotateLeft(node);
+        AVLNode* node = new AVLNode();
+        node->key = key;
+        node->Left = NULL;
+        node->Right = NULL;
+        node->height = 1; // новый узел добавлен как лист
+        return(node);
+    }
+
+    AVLNode* RRotate(AVLNode* y) // повернуть вправо поддерево с корнем y
+    {
+        AVLNode* x = y->Left;
+        AVLNode* T2 = x->Right;
+
+        // Выполнить вращение 
+        x->Right = y;
+        y->Left = T2;
+
+        // Высоты обновления  
+        y->height = max(GetSize(y->Left), GetSize(y->Right)) + 1;
+        x->height = max(GetSize(x->Left), GetSize(x->Right)) + 1;
+
+        // Вернуть новый корень 
+        return x;
+
+    }
+
+    AVLNode* LRotate(AVLNode* x) // повернуть влево поддерево с корнем x
+    {
+        AVLNode* y = x->Right;
+        AVLNode* T2 = y->Left;
+
+        // Выполнить вращение 
+        y->Left = x;
+        x->Right = T2;
+
+        // Обновить высоты
+        x->height = max(GetSize(x->Left), GetSize(x->Right)) + 1;
+        y->height = max(GetSize(y->Left), GetSize(y->Right)) + 1;
+
+        // Вернуть новый корень 
+        return y;
+
+    }
+
+    int Balance(AVLNode* node)
+    {
+        return GetSize(node->Left) - GetSize(node->Right);
+
+    }
+
+    AVLNode* insert(AVLNode* node, int key) 
+    {
+        if (!node) {//если узла нет то создадим
+            return newNode(key);
         }
-    }*/
+
+        if (key < node->key) 
+        {
+            node->Left = insert(node->Left, key);
+        }
+        else if (key > node->key) 
+        {
+            node->Right = insert(node->Right, key);
+        }
+        else 
+        {
+            return node; // дубликаты не разрешены
+        }
+
+        ///// 
+        
+        node->height = 1 + max(GetSize(node->Left), GetSize(node->Right));//тк мы докинули узел нужно обновить высоту
+
+        int balance = Balance(node);//проверка на баланс
+
+        // лево лево 
+        if (balance > 1 && key < node->Left->key) 
+        {
+            return RRotate(node);
+        }
+
+        // право право
+        if (balance < -1 && key > node->Right->key) 
+        {
+            return LRotate(node);
+        }
+
+        // лево право
+        if (balance > 1 && key > node->Left->key) 
+        {
+            node->Left = LRotate(node->Left);
+            return RRotate(node);
+        }
+
+        // право лево
+        if (balance < -1 && key < node->Right->key) 
+        {
+            node->Right = RRotate(node->Right);
+            return LRotate(node);
+        }
+
+        return node;
+
+    }
+
+    void insert(int key) 
+    {
+        root = insert(root, key);
+    }
+
+    AVLNode* deleteMin(AVLNode* node) {
+        if (node->Left == nullptr)
+        {
+            return node->Right;
+        }
+        node->Left = deleteMin(node->Left);
+        node->height = 1 + GetSize(node->Left) + GetSize(node->Right);
+        return node;
+    }
+
+    AVLNode* getMin(AVLNode* node) {
+
+       if (!node->Left) 
+       { 
+           return node;
+       }
+        return getMin(node->Left);
+    }
+
+  
+    AVLNode* deleteNode(AVLNode* root, int key)
+    {
+        if (root == NULL)
+            return root;
+
+        //удалить по BST 
+        if (key < root->key)
+            root->Left = deleteNode(root->Left, key);
+
+        else if (key > root->key)
+            root->Right = deleteNode(root->Right, key);
+
+        else
+        {
+            // узел только с одним дочерним элементом или без дочернего элемента  
+            if ((root->Left == NULL) ||
+                (root->Right == NULL))
+            {
+                AVLNode* temp = root->Left ?
+                    root->Left :
+                    root->Right;
+
+                if (temp == NULL)
+                {
+                    temp = root;
+                    root = NULL;
+                }
+                else
+                    *root = *temp;
+                free(temp);
+            }
+            else
+            {
+                AVLNode* temp = minValueNode(root->Right);
+
+                root->key = temp->key;
+
+                // Удалить преемника по порядку 
+                root->Right = deleteNode(root->Right,temp->key);
+            }
+        }
+
+        if (root == NULL)
+            return root;
+
+        // глубина обновления  
+        root->height = 1 + max(GetSize(root->Left),GetSize(root->Right));
+
+        // получить коэффициент баланса 
+        int balance = Balance(root);
+
+        //повернуть дерево, если оно несбалансировано
+
+        // Лево лево  
+        if (balance > 1 && Balance(root->Left) >= 0)
+            return RRotate(root);
+
+        // Лево право  
+        if (balance > 1 && Balance(root->Left) < 0) {
+            root->Left = LRotate(root->Left);
+            return RRotate(root);
+        }
+        // Право право  
+        if (balance < -1 && Balance(root->Right) <= 0)
+            return LRotate(root);
+
+        // Право лево  
+        if (balance < -1 && Balance(root->Right) > 0) {
+            root->Right = RRotate(root->Right);
+            return LRotate(root);
+        }
+        return root;
+    }
+
+    AVLNode* minValueNode(AVLNode* node) 
+    {
+        AVLNode* current = node;
+        while (current->Left) 
+        {
+            current = current->Left;
+        }
+        return current;
+    }
+
+    void deleteNode(int key) 
+    {
+        root = deleteNode(root, key);
+    }
+
+    AVLNode* search(AVLNode* root, int key) {
+        if (!root || root->key == key) {
+            return root;
+        }
+
+        if (key < root->key) {
+            return search(root->Left, key);
+        }
+        else {
+            return search(root->Right, key);
+        }
+    }
+
+    AVLNode* search(int key) 
+    {
+        return search(root, key);
+    }
+
+    void CurrentLevelCheck(AVLNode* root, int l) 
+    {
+        if (root == nullptr) {
+            return;
+        }
+        if (l == 0) {
+            cout << root->key<< " ";
+        }
+        else if (l > 0) 
+        {
+            CurrentLevelCheck(root->Left, l - 1);
+            CurrentLevelCheck(root->Right, l - 1);
+        }
+
+    }
+    void BFS(AVLNode* root) 
+    {
+        int h = GetSize(root);
+        for (int l = 0; l < h;l++) 
+        {
+            CurrentLevelCheck(root, l);
+        }
+    }
+
+    void BFS() {
+        BFS(root);
+        cout << "\n";
+    }
+    
+    void DFSPrefix() {
+        DFSPrefix(root);
+        cout << "\n";
+    }
+
+    void DFSInfix() {
+        DFSInfix(root);
+        cout << "\n";
+    }
+
+    void DFSPostfix() {
+        DFSPostfix(root);
+        cout << "\n";
+    }
+    void DFSPrefix(AVLNode* node) {
+       if (!node) return;
+        cout << node->key << " ";
+        DFSPrefix(node->Left);
+        DFSPrefix(node->Right);
+         }
+
+    void DFSInfix(AVLNode* node) {
+        if (!node) return;
+
+        DFSInfix(node->Left);
+        cout << node->key << " ";
+        DFSInfix(node->Right);
+         }
+
+    void DFSPostfix(AVLNode* node) {
+        if (!node) return;
+
+        DFSPostfix(node->Left);
+        DFSPostfix(node->Right);
+        cout << node->key << " ";
+         }
 };
 
+void AVLFromBin(Node* binaryNode, AVLTree& avlTree)
+{
+    if (binaryNode)
+    {
+        avlTree.insert(binaryNode->data);  
+        AVLFromBin(binaryNode->left, avlTree);
+        AVLFromBin(binaryNode->right, avlTree);
+    }
+}
 
 
 int main()
 {
+    AVLTree avl;
+   
     BinaryTree tree;
     string inputString;
     ifstream inputFile("C:\\Users\\83532\\source\\repos\\OOP_pw1\\Project2\\test.txt");
@@ -237,15 +555,29 @@ int main()
 
     if (!tree.isValidInput(line))
     {
-        cerr << "Invalid input format." << endl;
+        cerr << "Invalid input format" << endl;
         return 1;
     }
 
     tree.buildTreeFromString(line);
 
-    
+    AVLFromBin(tree.getRoot(), avl);
 
+    cout << "binary tree DFS Prefix traversal  ";
     tree.PrintDFS();
+
+    cout << "\nAVL Tree BFS traversal ";
+    avl.BFS();
+
+    cout << "\nAVL Tree DFS Prefix traversal ";
+    avl.DFSPrefix();
+
+    cout << "\nAVL Tree DFS Infix traversal ";
+    avl.DFSInfix();
+
+    cout << "\nAVL Tree DFS Postfix traversal ";
+    avl.DFSPostfix();
+
 
     return 0;
 }
